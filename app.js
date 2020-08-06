17bf3e7a43080cad686afe99c1c91f7cbd7231fd
alekseyevd <alekseyev.d@gmail.com>
2020-08-07 02:22:36 +0300

    dist

--- app.js
+++ app.js
@@ -10,9 +10,13 @@ app.use(function(req, res, next) {
   next();
 });
 
+const PORT = process.env.PORT || 80;
+
 app.use('/api/palindrome', require('./routes/palindrome'))
 app.use('/api/generate', require('./routes/generate'))
 
-const PORT = process.env.PORT || 80;
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
+})
 
 app.listen(PORT, () => console.log(`App has been started on port ${PORT}`))
